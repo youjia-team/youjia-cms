a1dd1fca3222e11662773c328610b85926d086a6
SSypp <2435884205@qq.com>
2019-07-27 15:53:38 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    
+    <HelloWorld msg="Hello!欢迎您参与本项目开发"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
