7261cefb3cc483c7dba50ef8d82337aacf39aa6d
rongpengfei <980236140@qq.com>
2019-07-30 13:48:52 +0800

    请求 axios 插件 和util 工具包

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './Login.vue'
+import axios from 'axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+axios.defaults.baseURL = 'http://localhost:8088';
+axios.defaults.headers.common['content-security-policy'] = "default-src 'self'"
+Vue.prototype.$axios = axios
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
